ea6313b568301b3c11ddf962fdbdb7e099eb1dfa
LieseCordeyn <liese.cordeyn@student.ehb.be>
2021-10-18 20:57:28 +0200

    technical test
    got data from my chosen API

---
+++ technicalTest/index.js
@@ -0,0 +1,20 @@
+"use strict"
+
+let htmlString = ""
+
+window.onload = function () {
+    let genre = "romance";
+
+    fetch(`https://openlibrary.org/subjects/${genre}.json`)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data.works)
+
+            data.works.forEach(book => {
+                htmlString += `<div> <p> ${book.title} </p></div>`
+
+            });
+
+            document.getElementById("content").innerHTML = htmlString;
+        })
+}
